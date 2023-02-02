68415e1261ce62b497e6279bb52b0006640927d9
Chiasmera <98319072+Chiasmera@users.noreply.github.com>
2023-02-02 14:23:08 +0100

    23-02-02 done

--- Semester_3/23-02-02_Functions/2_6.js
+++ Semester_3/23-02-02_Functions/2_6.js
@@ -5,3 +5,31 @@
 //parentes er øverst på stakken. Brug metoderne push() og pop().
 //→ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
 
+let trueCode = 'let sum = function (array){ let sum = 0; for (const value of array) { sum += value; } return sum;}';
+let falseCode = 'let sum = function (array){ let sum = 0; for (const value of array}) sum += value; } return sum;}}}}}';
+
+function isBalanced(codeSnippet) {
+    let returnValue = true;
+    let balanceStack = [];
+    const beginners = ['(', '{', '['];
+    const enders = [')', '}', ']'];
+    for (const value of codeSnippet.split('')) {
+        if (beginners.includes(value)) {
+            balanceStack.push(value);
+
+        } else if (enders.includes(value)) {
+            let popped = balanceStack.pop();
+            if (typeof popped === 'undefined' || enders.indexOf(popped) != beginners.indexOf(value)) {
+                returnValue = false;
+                break;
+            }
+        }
+    }
+    if (balanceStack.length > 0) {
+        returnValue = false;
+    }
+    return returnValue;
+}
+
+console.log(isBalanced(trueCode));
+console.log(isBalanced(falseCode));
--- Semester_3/23-02-02_Functions/2_7.js
+++ Semester_3/23-02-02_Functions/2_7.js
@@ -1,3 +1,41 @@
 //Lav et array med tal.
 //Modificer dernæst max(), contains() og sum() funktionerne fra opgave 2.1, så de bliver metoder på dette
 //array.
+
+let numberArray = [7, 12, 13, 3, 6, 2, 2, 9, 7, 13, 26, 3, 9];
+
+numberArray.max = function () {
+    
+    let currentMax = -1;
+
+    for (const element of numberArray) {
+            if (element > currentMax) {
+                currentMax = element;
+            }
+    };
+    return currentMax;
+};
+
+numberArray.contains = function (element) {
+    for ( const value of numberArray) {
+        if (value === element) {
+            return true;
+        }
+    }
+    return false;
+
+}
+
+numberArray.sum = function () {
+    let sum = 0;
+    for (const value of numberArray) {
+        sum += value;
+    }
+    return sum;
+}
+
+console.log(`array er: ${numberArray}`)
+console.log(`array indeholder ${numberArray.max()} som max tal`)
+console.log(`array indeholder tallet 2: ${numberArray.contains(2)}`)
+console.log(`array indeholder tallet 1: ${numberArray.contains(1)}`)
+console.log(`summen af array er: ${numberArray.sum()}`)
