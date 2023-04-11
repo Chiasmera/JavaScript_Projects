4aff3c09856f22c6ebfea76032935151f779ebb2
Chiasmera <98319072+Chiasmera@users.noreply.github.com>
2023-04-11 15:44:17 +0200

    23-04-11 15,44

--- EgneProjekter/ShelfHelp/app.js
+++ EgneProjekter/ShelfHelp/app.js
@@ -1,3 +1,4 @@
+//IMPORTS  -------------------------------------------------------------------------------------------
 //Express import
 import express, { response } from 'express'
 const app = express()
@@ -5,10 +6,41 @@ const app = express()
 //Firebase import
 import { initializeApp } from 'firebase/app'
 import { getFirestore, collection, getDocs, getDoc, doc, deleteDoc, addDoc } from 'firebase/firestore'
+import { firebaseConfig } from './FB_Config.js'
+
+const firebase_app = initializeApp(firebaseConfig)
+const db = getFirestore(firebase_app)
 
 //Pug import
 import pug from 'pug'
 app.set('view engine', 'pug')
 
-//Middleware
+
+//global variables --------------------------------------------------------------------------------------
+const gamecollection = collection(db, 'GameCollection')
+
+//Middleware  -------------------------------------------------------------------------------------------
 app.use(express.static('assets'))
+
+
+//Endpoints
+app.get('/', async (req, res)=> {
+    const games = await getGames();
+    res.render('home', {games: games})
+    
+})
+
+
+//Functions----------------------------------------------------------------------------------------------
+async function getGames() {
+    const gameDocs = await getDocs(gamecollection)
+    const games = gameDocs.docs.map( (gameDoc)=> {
+        let data = gameDoc.data()
+        data.objectID = gameDoc.id
+        return data
+    })
+    return games
+}
+
+//Server start  -----------------------------------------------------------------------------------------
+app.listen(3141, ()=> console.log('server running on port 3141'))
