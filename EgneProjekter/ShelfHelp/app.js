9819b29374a374b639d6046c271c66cc62050b64
Chiasmera <98319072+Chiasmera@users.noreply.github.com>
2023-04-21 19:53:37 +0200

    sync complete

--- EgneProjekter/ShelfHelp/app.js
+++ EgneProjekter/ShelfHelp/app.js
@@ -59,15 +59,19 @@ async function getGames() {
 async function getDBGamesIDs () {
     const gameDocs = await getDocs(dbGameCollection)
     const gamesIDs = gameDocs.docs.map( (gameDoc)=> {
-        data.objectID = gameDoc.id
-        return data
+        return gameDoc.id
     })
     return gamesIDs
 }
 
-async function addGameToDB (game) {
-    let docRef = doc (db, 'GameCollection', game.objectID)
-    docRef = await setDoc(docRef, game, {merge:true})
+async function addGameToDB (gameObject) {
+    const id = gameObject.items.item['@_id']
+    await setDoc(doc(db, "GameCollection", String(id)), gameObject, {merge: true});
+
+}
+
+async function deleteGameFromDB(objectID) {
+    await deleteDoc(doc(db, 'GameCollection', objectID)) 
 }
 
 async function fetchXML(url) {
@@ -89,6 +93,12 @@ async function parseXMLToObject(XML) {
     return gameObject
 }
 
+async function getGameObject (objectID) {
+    const gameXML = await fetchXML(`https://boardgamegeek.com/xmlapi2/thing?id=${objectID}`)
+    let gameObject = await parseXMLToObject(gameXML)
+    return gameObject
+}
+
 async function bggSync (username) {
     let removedGames = 0;
     let addedGames = 0;
@@ -102,43 +112,44 @@ async function bggSync (username) {
     const dbIDs = await getDBGamesIDs();
 
     //Remove from DB those not in collection.
-    dbIDs.array.forEach(dbID => {
-        const index = bggIDs.indexOf(dbID)
-        if (index === -1) {
-            //remove this game from DB
-            //TODO 
-                       
-        } else {
-            //remove ID from bggIDs
-            bggIDs.splice(index, 1)
-        }
-    });
-    //Rest of bggIDS are not in DB:
-            //fetch data from BGG
-          //  const gameXML = await fetchXML(`https://boardgamegeek.com/xmlapi2/thing?id=${objectid}`)
-          //  const gameObject = await parseXMLToObject(gameXML)
-            //add object to DB
-         //   addGameToDB(gameObject)
-            
 
+    if (Array.isArray(dbIDs) && Array.isArray(bggIDs)) {
+        dbIDs.forEach(async (dbID)=> {
+            if (!bggIDs.includes(dbID)) {
+                //remove this game from DB
+                await deleteGameFromDB(dbID)
+                removedGames++
+            }
+        })
+
+        bggIDs.forEach( async (bggID) => {
+            if (!dbIDs.includes(bggID)) {
+                //fetch data from BGG
+                const gameObject = await getGameObject(bggID)
+                //add object to DB
+                await addGameToDB(gameObject)
+                addedGames++
+            }
+        })
+        
+    } else {
+        console.log('dbIDs or bggIDs is not an array, but it should be');
+    }
     console.log(`BGG sync complete. ${addedGames} games added to collection, ${removedGames} games removed`);
 }
 
 
 //Server start  -----------------------------------------------------------------------------------------
 
-
-const tempGame = {title: 'testosterone', objectID: '12345678'}
-let id = addGameToDB(tempGame)
-
 async function main () {
-    //7 wonders object id = 173346
+
     await bggSync(username)  
     
 }
 
 
 app.listen(3141, ()=> console.log('server running on port 3141'))
+
 main()
 
 
