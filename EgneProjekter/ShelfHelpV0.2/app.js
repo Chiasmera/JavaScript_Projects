36cea8bf24c7d0d5999ff6b5e1e9102e00a9b4eb
Chiasmera <98319072+Chiasmera@users.noreply.github.com>
2023-06-04 14:43:07 +0200

    game list sorted

--- EgneProjekter/ShelfHelpV0.2/app.js
+++ EgneProjekter/ShelfHelpV0.2/app.js
@@ -22,8 +22,8 @@ app.set('view engine', 'pug')
 app.use(express.static('assets'))
 
 app.get('/', async (req, res) => {
-    const allGames = await getGamesFromDB()
-    res.render('gameListView', {games: allGames})
+    let allGames = await getGamesFromDB()
+    res.render('gameListView', {games: allGames, sortBy: sortBy})
 })
 
 app.get('/shelf', (req, res) => {
@@ -31,6 +31,20 @@ app.get('/shelf', (req, res) => {
     res.render('shelfOrganizerView', {test: testGetGame})
 })
 
+function sortBy (list, property) {
+    let sample = list[0]
+    if (property === 'title') {
+        list.sort( (a, b) => {return a['title'].localeCompare(b['title'])})
+    } else if (property === 'rating') {
+        list.sort( (a, b) => {return b['averageRating'] - a['averageRating']})
+    }  else if (property === 'time') {
+        list.sort( (a, b) => {return ((a.minTime + a.maxTime)/2) - ((b.minTime + b.maxTime)/2)})
+    } else if (typeof sample[property] === 'string') {
+        list.sort( (a, b) => {return a[property].localeCompare(b[property])})
+    } else {
+        list.sort( (a, b) => {return a[property] - b[property]})
+    }
+}
 
 async function synchronizeWithDB () {
     console.log('Synchronizing with BGG');
